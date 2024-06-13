9ac22779129cad3e5e89b9f1830b4a897fc09492
ElvineJoy <elvinkuiyungest@gmail.com>
2024-06-13 18:19:12 +0300

    First test pass

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
 // Your code here
+// for each vowel. +1 points
+// for each consonant +2 points
+export function pointsForWord(word) {
+    let points = 0;
+    for(const char of word) {
+        if (["a","b", "c", "d", "e"].includes(char)) {
+            points += 1;
+        } else {
+            points +=2;
+        }
+    }
+    return points;
+}
